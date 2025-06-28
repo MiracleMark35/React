0dfa0957ecc0c9580af2855775ae1de8b68b9cff
MiracleMark35 <markianfrost123@gmail.com>
2025-06-28 19:43:19 +0800

    1 hour

---
+++ my-vite-project/src/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cards (children) {
+  return (
+   <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
+    {children}
+   </div>
+  )
+}
+
+export default Cards
